c7065105ad01b7284e780264c9974d59f746802a
jd-carpio <carpiohu@grinnell.edu>
2026-02-13 10:19:48 -0600

    fri

--- src/main/java/edu/grinnell/csc207/lists/LinkedList.java
+++ src/main/java/edu/grinnell/csc207/lists/LinkedList.java
@@ -4,20 +4,60 @@ package edu.grinnell.csc207.lists;
  * A linked implementation of the list ADT.
  */
 public class LinkedList {
+
+    private static class Node {
+        public int value;
+        public Node next;
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+        
+        /**
+         * 
+             * @param value gives a value to the node
+         */
+        public Node(int value) {
+            this(value, null);
+        }
+    }
+
+    private Node first;
+
+    public LinkedList() {
+        first = null;
+    }
+
+
     /**
      * Adds <code>value</code> to the end of the list
      * 
      * @param value the value to add to the end of the list
      */
     public void add(int value) {
-        throw new UnsupportedOperationException("Unimplemented method 'add'");
+        Node newNode = new Node(value,null);
+        if (this.first == null) {
+            this.first = newNode;
+        } else {
+            Node current = this.first;
+            for(; current.next == null; current = current.next);
+            current.next =newNode;
+        }
     }
 
     /**
      * @return the number of elements in the list
      */
     public int size() {
-        throw new UnsupportedOperationException("Unimplemented method 'size'");
+        int count = 0;
+        if (this.first == null) {
+            count = 0;
+        } else {
+            for(Node current = this.first; current.next == null; current = current.next);
+            count++;
+        }
+        return count;
     }
 
     /**
