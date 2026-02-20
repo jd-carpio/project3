14e088a08db53093ddabb770e961be903537ccec
jd-carpio <carpiohu@grinnell.edu>
2026-02-20 14:04:57 -0600

    part1

--- src/main/java/edu/grinnell/csc207/lists/ArrayList.java
+++ src/main/java/edu/grinnell/csc207/lists/ArrayList.java
@@ -4,20 +4,31 @@ package edu.grinnell.csc207.lists;
  * An array-based implementation of the list ADT.
  */
 public class ArrayList {
+    int[] data ;
+    int size;
+
+    private void ensureCapacity() {
+        if (size == data.length) {
+            data = Arrays.copyOf(data, data.length * 2);
+        }
+    }
+
     /**
      * Adds <code>value</code> to the end of the list
      * 
      * @param value the value to add to the end of the list
      */
     public void add(int value) {
-        throw new UnsupportedOperationException("Unimplemented method 'add'");
+        ensureCapacity();
+        this.data[size] = value;
+        size++;
     }
 
     /**
      * @return the number of elements in the list
      */
     public int size() {
-        throw new UnsupportedOperationException("Unimplemented method 'size'");
+        return size;
     }
 
     /**
@@ -25,7 +36,10 @@ public class ArrayList {
      * @return the value at the specified <code>index</code>
      */
     public int get(int index) {
-        throw new UnsupportedOperationException("Unimplemented method 'get'");
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        return this.data[index];
     }
 
     /**
@@ -35,6 +49,16 @@ public class ArrayList {
      * @return the element at <code>index</code>
      */
     public int remove(int index) {
-        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+    }
+    int val = this.data[index];
+        
+    for(int i = index; i < size - 1; i++) {
+        data[i] = data[i + 1];
+    }
+
+    size--;
+    return val;    
     }
 }
