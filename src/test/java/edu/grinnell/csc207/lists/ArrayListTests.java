84e7a6cb60430d8d98a8eecd33a6a16825683d1a
jd-carpio <carpiohu@grinnell.edu>
2026-02-20 18:29:47 -0600

    part3

--- src/test/java/edu/grinnell/csc207/lists/ArrayListTests.java
+++ src/test/java/edu/grinnell/csc207/lists/ArrayListTests.java
@@ -16,11 +16,34 @@ public class ArrayListTests {
     }
 
     @Test
+    public void removeMiddleElement() {
+        Array list = new ArrayList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        int removed = list.remove(1);
+        assertEquals(2, list.size());
+        assertEquals(3, list.get(1));
+    }
+
+    @Test
     public void listAddSize10() {
         ArrayList list = new ArrayList();
         for (int i = 0; i < 10; i++) {
             list.add(i);
         }
+        assertEquals(10, list.size());
+    }
+
+    @Test
+    public void listTestRemove() {
+        ArrayList list = new ArrayList();
+        for (int i = 0; i < 15; i++) {
+            list.add(i);
+        }
+        for (int i = 0; i < 5; i++) {
+            list.remove(i);
+        }  
         assertEquals(10, list.size());
     }
 
--- src/test/java/edu/grinnell/csc207/lists/LinkedListTests.java
+++ src/test/java/edu/grinnell/csc207/lists/LinkedListTests.java
@@ -24,6 +24,29 @@ public class LinkedListTests {
         assertEquals(10, list.size());
     }
 
+    @Test
+    public void listTestRemove() {
+        Linked list = new LinkedList();
+        for (int i = 0; i < 15; i++) {
+            list.add(i);
+        }
+        for (int i = 0; i < 5; i++) {
+            list.remove(i);
+        }  
+        assertEquals(10, list.size());
+    }
+
+    @Test
+    public void removeMiddleElement() {
+        LinkedList list = new LinkedList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        int removed = list.remove(1);
+        assertEquals(2, list.size());
+        assertEquals(3, list.get(1));
+    }
+
     @Property
     public boolean listAddSize(@ForAll @IntRange(min = 0, max = 1000) int sz) {
         LinkedList list = new LinkedList();
